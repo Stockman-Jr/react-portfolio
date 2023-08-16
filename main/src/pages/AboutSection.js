cf46231a142d8c1d1c9e0fdbb8fb8d77e473017b
Stockman-Jr <stockmanmimmi@gmail.com>
2023-08-16 13:12:44 +0200

    Add styles and skillbars to about section

--- main/src/pages/AboutSection.js
+++ main/src/pages/AboutSection.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "../styles/Sections.module.css";
 import SvgBorders from "../components/SvgBorders";
+import SkillBar from "../components/SkillBar";
 
 function AboutSection(props) {
   const { currentPage } = props;
@@ -8,6 +9,26 @@ function AboutSection(props) {
     <section className={styles.Main}>
       <div className={styles.InnerContainer}>
         <h1>AboutSection</h1>
+        <div className={styles.Skills}>
+          <div className={styles.Bars}>
+            <span>HTML</span>
+            <SkillBar progress={95} currentPage={currentPage} />
+            <span>CSS</span>
+            <SkillBar progress={90} currentPage={currentPage} />
+            <span>Bootstrap</span>
+            <SkillBar progress={90} currentPage={currentPage} />
+            <span>Python</span>
+            <SkillBar progress={80} currentPage={currentPage} />
+            <span>Django</span>
+            <SkillBar progress={80} currentPage={currentPage} />
+            <span>JavaScript</span>
+            <SkillBar progress={80} currentPage={currentPage} />
+            <span>JQuery</span>
+            <SkillBar progress={80} currentPage={currentPage} />
+            <span>React</span>
+            <SkillBar progress={70} currentPage={currentPage} />
+          </div>
+        </div>
       </div>
     </section>
   );
